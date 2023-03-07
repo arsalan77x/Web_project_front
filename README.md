08d156988434338ee240d4fbb832c7fd45b745fa
arsalan77x <arsalan77x@gmail.com>
2023-03-07 23:15:56 +0330

    components and home

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Web_project_front
+
+# Bastani tarasht online shop
---
+++ src/Data/APP_URL.js
@@ -0,0 +1,7 @@
+// const APP_URL = "http://192.168.1.100:3001/api/v2"
+const APP_URL = "http://localhost:3001/api/v2"
+export default APP_URL
+export const APP_TOKEN = "GCMUDiuY5a7WvyUNt9n3QztToSHzK7Uj"
+export const MAX_MOBILE_WIDTH = "576"
+
+
